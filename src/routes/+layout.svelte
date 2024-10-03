8b3c90b1bd465fd10ece51ae6fd18f36bd1b7d70
tyayers <tyler.ayers@gmail.com>
2024-10-03 11:25:02 +0200

    added page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
-	import '../app.css';
+	import { fade, fly } from "svelte/transition";
+
+	import "../app.css";
+
+	export let data;
 
 	function sendCancel() {
 		//First, we initialize our event
-		const event = new Event('cancelEvent');
+		const event = new Event("cancelEvent");
 		// Next, we dispatch the event.
 		document.dispatchEvent(event);
 	}
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div id="layout_box" on:click={sendCancel} on:keyup={sendCancel}>
-
-  <slot />
-
-</div>
+{#key data.url}
+	<div id="layout_box" on:click={sendCancel} on:keyup={sendCancel} in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+	<!-- <div id="layout_box" on:click={sendCancel} on:keyup={sendCancel} in:fly={{ x: 50, duration: 150, delay: 150 }} out:fly={{ x: -50, duration: 150 }}> -->
+		<slot />
+	</div>
+{/key}
 
 <style>
 	#layout_box {
